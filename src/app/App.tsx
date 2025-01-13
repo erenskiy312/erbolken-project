ac120703ef8c699dc313d59080c7d903710ed2a9
erbolken <msuharik2@gmail.com>
2025-01-13 19:02:07 +0300

    feat: implemented FSD architecture and added absolute imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { Suspense, useContext, useState } from "react"
 import { Route, Routes, Link } from "react-router-dom"
 import "./styles/index.scss"
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
-import { MainPageAsync } from "./pages/MainPage/MainPage.async"
-import { useTheme } from "./theme/useTheme"
-import { classNames } from "./helpers/classNames/classNames"
+import { AboutPageAsync } from "pages/AboutPage/ui/AboutPage.async"
+import { MainPageAsync } from "pages/MainPage/ui/MainPage.async"
+import { useTheme } from "./providers/ThemeProvider"
+import { AboutPage } from "pages/AboutPage"
+import { MainPage } from "pages/MainPage"
+import { classNames } from "shared/lib/classNames/classNames"
 
 export default function App() {
     const {theme, toggleTheme} = useTheme()
@@ -16,8 +18,8 @@ export default function App() {
         <Link to={"/about"}>О сайте</Link>
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                <Route path={"/about"} element={<AboutPageAsync/>}/>
-                <Route path={"/"} element={<MainPageAsync/>}/>
+                <Route path={"/about"} element={<AboutPage/>}/>
+                <Route path={"/"} element={<MainPage/>}/>
             </Routes>
       </Suspense>
     </div>
