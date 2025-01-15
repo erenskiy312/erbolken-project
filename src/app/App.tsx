e139e54640f7df7ce9aa0423206ddd38feb0fe77
erbolken <msuharik2@gmail.com>
2025-01-15 23:33:56 +0300

    refactor: configuration react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,8 @@
-import { Suspense, useContext, useState } from "react"
-import { Route, Routes, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import "./styles/index.scss"
-import { AboutPageAsync } from "pages/AboutPage/ui/AboutPage.async"
-import { MainPageAsync } from "pages/MainPage/ui/MainPage.async"
 import { useTheme } from "./providers/ThemeProvider"
-import { AboutPage } from "pages/AboutPage"
-import { MainPage } from "pages/MainPage"
 import { classNames } from "shared/lib/classNames/classNames"
+import { AppRouter } from "./providers/router"
 
 export default function App() {
     const {theme, toggleTheme} = useTheme()
@@ -16,12 +12,7 @@ export default function App() {
         <button onClick={toggleTheme}>THEME</button>
         <Link to={"/"}>Главная</Link>
         <Link to={"/about"}>О сайте</Link>
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                <Route path={"/about"} element={<AboutPage/>}/>
-                <Route path={"/"} element={<MainPage/>}/>
-            </Routes>
-      </Suspense>
+        <AppRouter/>
     </div>
   )
 }
