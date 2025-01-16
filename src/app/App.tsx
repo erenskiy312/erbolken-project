1cc30ef9730b51faa7dec64378b06e1b399b787c
erbolken <msuharik2@gmail.com>
2025-01-16 20:41:03 +0300

    feat: add first widget components: Navbar. add first ui-kit component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
-import { Link } from "react-router-dom"
 import "./styles/index.scss"
 import { useTheme } from "./providers/ThemeProvider"
 import { classNames } from "shared/lib/classNames/classNames"
 import { AppRouter } from "./providers/router"
+import { Navbar } from "widgets/Navbar"
 
 export default function App() {
     const {theme, toggleTheme} = useTheme()
 
   return (
     <div className={classNames("app", {}, [theme])}>
-        <button onClick={toggleTheme}>THEME</button>
-        <Link to={"/"}>Главная</Link>
-        <Link to={"/about"}>О сайте</Link>
+        <Navbar/>
         <AppRouter/>
+        <button onClick={toggleTheme}>THEME</button>
     </div>
   )
 }
