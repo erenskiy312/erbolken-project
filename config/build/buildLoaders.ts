258bb38435a0e494265f4800b851d49667ed70eb
erbolken <msuharik2@gmail.com>
2025-01-17 21:05:21 +0300

    feat: download svg loader, import loader, add Button UI kit implemented icons for themes

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,12 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -34,7 +40,18 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+    }
+
     return [
+        fileLoader,
+        svgLoader,
         cssLoader,
         typescriptLoader
     ]
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,12 @@ import { AppRouter } from "./providers/router"
 import { Navbar } from "widgets/Navbar"
 
 export default function App() {
-    const {theme, toggleTheme} = useTheme()
+    const { theme } = useTheme()
 
   return (
     <div className={classNames("app", {}, [theme])}>
         <Navbar/>
         <AppRouter/>
-        <button onClick={toggleTheme}>THEME</button>
     </div>
   )
 }
