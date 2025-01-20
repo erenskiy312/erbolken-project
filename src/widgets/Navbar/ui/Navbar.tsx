b8e3c75d3bb4935dd9386a96d7052ed431f61799
erbolken <msuharik2@gmail.com>
2025-01-20 23:45:58 +0300

    feat: add i18n, add russian and eenglish languages, decomposed json files for languages

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
@@ -8,6 +9,8 @@ interface NavbarProps {
 }
 
 export const Navbar = ({className}: NavbarProps) => {
+    const {t} = useTranslation()
+    
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
