61cd67f822b434aa72ed52b771a6b947f17169e0
erbolken <msuharik2@gmail.com>
2025-02-01 23:56:28 +0600

    add Error Boundary, create ui component for catching errors from Error Boundary

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,13 @@ import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { AppRouter } from './providers/router';
 import { useTheme } from './providers/ThemeProvider';
 
 export default function App() {
   const { theme } = useTheme();
-
+  
   return (
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback="">
---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,27 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './BugButton.module.scss';
+import Button from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+
+// Компонент для тестирования Error Boundary
+export const BugButton = () => {
+    const [error, setError] = useState(false)
+    const { t } = useTranslation()
+
+    const onThrow = () => setError(true)
+
+    useEffect(() => {
+        if(error) {
+            throw new Error()
+        }
+    }, [error])
+
+    return (
+        <Button onClick={onThrow}>
+            {t('Воспроизведение ошибки')}
+        </Button>
+    );
+};
+
+export default BugButton;
