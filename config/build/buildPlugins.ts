c4d677e48f2546abc829c8b81e3e57428ffbbb75
erbolken <msuharik2@gmail.com>
2025-02-11 19:06:43 +0600

    change: fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,25 +6,30 @@ import { BuildOptions } from './types/config';
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer"
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
-    new HtmlWebpackPlugin({
-      template: paths.html,
-    }),
+    const plugins = [
+      new HtmlWebpackPlugin({
+        template: paths.html,
+      }),
+  
+      new webpack.ProgressPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+      }),
+  
+      new webpack.DefinePlugin({
+        __IS_DEV__: JSON.stringify(isDev),
+      }),
+    ]
 
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
+    if(isDev){
+      plugins.push(new webpack.HotModuleReplacementPlugin());
+      plugins.push(new ReactRefreshWebpackPlugin())
+      plugins.push(new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      })) 
+    }
+    return plugins
 
-    new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev),
-    }),
-
-    ...(isDev ? [new webpack.HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()] : []),
-
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
-  ];
+    
 }
